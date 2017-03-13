3166a91f94e882b837453e03e49bc73ced8d8937
laure <laure.joye@gmail.com>
2017-03-13 11:49:21 +0100

    Modification dans SecurityController.php
    modification des use

--- src/OCUserBundle/Controller/SecurityController.php
+++ src/OCUserBundle/Controller/SecurityController.php
@@ -5,8 +5,6 @@ namespace OCUserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 
 class SecurityController extends Controller
