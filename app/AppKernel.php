5eb0756e4b2efe93134bb1b0871ef0755660fe4b
laure <laure.joye@gmail.com>
2017-03-13 12:19:31 +0100

    modification du AppKernel pour installation du DataFixtureBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,11 +21,12 @@ class AppKernel extends Kernel
             new OCUserBundle\OCUserBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'))){
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
