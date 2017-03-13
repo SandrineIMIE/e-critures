934e916f39320772eff165d05eae5d0fe55bdeae
sandrine Micoulaut <s.micoulaut@gmail.com>
2017-03-13 11:53:54 +0100

    creation crud

---
+++ src/ProjectBundle/Form/LexicomType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ProjectBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LexicomType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('mot')->add('definition')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProjectBundle\Entity\Lexicom'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'projectbundle_lexicom';
+    }
+
+
+}
