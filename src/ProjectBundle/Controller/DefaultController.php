379dc5e26ff0c4ccb74d387dc44969f642f8f536
laure <laure.joye@gmail.com>
2017-03-14 14:29:36 +0100

    modifications liées à la suppression des fichiers

--- src/ProjectBundle/Controller/DefaultController.php
+++ src/ProjectBundle/Controller/DefaultController.php
@@ -2,13 +2,15 @@
 
 namespace ProjectBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/home", name="homepage")
      */
     public function indexAction()
     {
