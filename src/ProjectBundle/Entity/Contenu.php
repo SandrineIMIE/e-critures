fbf7c07f31ab8ecd49d90338707c901f909701f8
sandrine Micoulaut <s.micoulaut@gmail.com>
2017-03-17 10:39:26 +0100

    creation version contenu

---
+++ src/ProjectBundle/Entity/Contenu.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace ProjectBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contenu
+ *
+ * @ORM\Table(name="contenu")
+ * @ORM\Entity(repositoryClass="ProjectBundle\Repository\ContenuRepository")
+ */
+class Contenu
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="versionat", type="datetime")
+     */
+    private $versionat;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Contenu
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * Set versionat
+     *
+     * @param \DateTime $versionat
+     *
+     * @return Contenu
+     */
+    public function setVersionat($versionat)
+    {
+        $this->versionat = $versionat;
+
+        return $this;
+    }
+
+    /**
+     * Get versionat
+     *
+     * @return \DateTime
+     */
+    public function getVersionat()
+    {
+        return $this->versionat;
+    }
+}
+
