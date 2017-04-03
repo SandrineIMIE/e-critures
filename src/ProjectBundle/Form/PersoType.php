0d2c7f2f35e41e305599e00879315f610e96f20f
laure <laure.joye@gmail.com>
2017-04-03 14:55:09 +0200

    Modification pour file upload

--- src/ProjectBundle/Form/PersoType.php
+++ src/ProjectBundle/Form/PersoType.php
@@ -5,6 +5,7 @@ namespace ProjectBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PersoType extends AbstractType
 {
@@ -21,7 +22,11 @@ class PersoType extends AbstractType
             ->add('lieun')
             ->add('sexe')
             ->add('statut')
-            ->add('mineur')        ;
+            ->add('mineur')
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,])
+
+            ;
     }
     
     /**
